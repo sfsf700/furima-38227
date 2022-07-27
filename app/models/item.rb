7566266a54d67cb52b1b66a1e9719a3e02a447eb
sfsf700 <hirokazu.2rnk@gmail.com>
2022-07-27 20:23:38 +0900

    商品出品機能の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,8 +17,9 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
+    validates :price, numericality: { only_integer: true,
+       greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
   
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -92,6 +92,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
+      it "価格に半角数字以外が含まれている場合は出品できない" do
+        @item.price = 'ゴールド'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
       it 'ユーザーが紐付いていないと保存できない' do
         @item.user = nil
         @item.valid?
