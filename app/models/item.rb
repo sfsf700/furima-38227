7fa3ad55bf2b4d341ae420ef1400080abbe65ec8
sfsf700 <hirokazu.2rnk@gmail.com>
2022-07-28 11:48:35 +0900

    商品一覧表示機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :days
   belongs_to :user
   has_one_attached :image
+  has_one :order
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
