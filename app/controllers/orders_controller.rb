ca56e018ad260472726c88ed80da5afd51756121
sfsf700 <hirokazu.2rnk@gmail.com>
2022-08-02 11:51:00 +0900

    環境変数の設定変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,25 +10,29 @@ class OrdersController < ApplicationController
 
   def create
     @order_form = OrderForm.new(order_params)
-    
     if @order_form.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price, 
-        card: order_params[:token],
-        currency: 'jpy' 
-      )
+      pay_item
       @order_form.save
       redirect_to root_path
     else
       render :index
     end
   end
+  
   private
 
   def order_params
     params.require(:order_form).permit(:postal_code, :area_id, :city, :house_number, :building_name, 
       :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price, 
+      card: order_params[:token],
+      currency: 'jpy' 
+    )
   end
 
   def purchased_item
